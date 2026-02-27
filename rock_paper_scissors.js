9ff302f84ed6f3626ee6af3f4b6f64bf57224e67
LLM97430 <130650980+LLM97430@users.noreply.github.com>
2026-02-28 00:54:48 +0100

    So I'm already done. I have to modify my code to make the message dynamic. (Explain you win/lose and why ex : "paper beat rock"). Then I need to create a playGame function, which include 5 rounds.

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,20 +1,53 @@
-const getComputerChoice = () => {
- let computerChoice = Math.floor(Math.random()*3);
- if (computerChoice == 0) {
-  console.log("Rock");
+let humanScore = 0;
+let computerScore = 0;
+
+function humanWin() {
+ console.log("You win !");
+ humanScore++;
+}
+
+function computerWin() {
+ console.log("You loose !")
+ computerScore++;
+}
+
+function getComputerChoice() {
+ let randomNumber = Math.floor(Math.random()*3);
+ if (randomNumber == 0) {
+  return "rock";
  }
- else if (computerChoice == 1) {
-  console.log("Paper");
+ else if (randomNumber == 1) {
+  return "paper";
  }
  else {
-  console.log("Scissors")
+ return "scissors";
  }
 }
-getComputerChoice();
 
-const getHumanChoice = () => {
- humanChoice = prompt("Choose Rock, Paper or Scissors");
- console.log(humanChoice)
+function getHumanChoice() {
+ return prompt("Choose Rock, Paper or Scissors").toLowerCase();
 }
 
-getHumanChoice();
+function playRound(humanChoice, computerChoice) {
+ humanChoice = getHumanChoice();
+ computerChoice = getComputerChoice(); 
+ if (getHumanChoice == getComputerChoice) {
+  console.log("It's a draw! Play again");
+ }
+ else if (getHumanChoice == "rock" && getComputerChoice == "scissors")
+ {
+  humanWin();
+ }
+ else if (getHumanChoice == "paper" && getComputerChoice == "rock") {
+  humanWin();
+ }
+ else if (getHumanChoice == "scissors" && getComputerChoice == "paper") {
+  humanWin();
+ }
+ else {
+  computerWin();
+ }
+
+ console.log(humanChoice);
+ console.log(computerChoice)
+}
