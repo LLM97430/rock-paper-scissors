531a14435e9ea16769e045cd36389ce1051cdb39
LLM97430 <130650980+LLM97430@users.noreply.github.com>
2026-02-28 00:03:42 +0100

    I have two functions now. One for the computer's choice. The second for the human's choice.

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,1 +1,20 @@
-console.log("Hello World!")
+const getComputerChoice = () => {
+ let computerChoice = Math.floor(Math.random()*3);
+ if (computerChoice == 0) {
+  console.log("Rock");
+ }
+ else if (computerChoice == 1) {
+  console.log("Paper");
+ }
+ else {
+  console.log("Scissors")
+ }
+}
+getComputerChoice();
+
+const getHumanChoice = () => {
+ humanChoice = prompt("Choose Rock, Paper or Scissors");
+ console.log(humanChoice)
+}
+
+getHumanChoice();
